208fe6ffe7b335132b75d11ccbdc04839e3e4488
Anton Karazeev <anton.karazeev@gmail.com>
2018-05-13 19:39:01 +0300

    Up main_telegram

--- main_telegram.py
+++ main_telegram.py
@@ -13,8 +13,13 @@ logger.setLevel(logging.DEBUG)
 info_text = []
 info_text.append('You can control me by sending these commands:')
 info_text.append('')
-info_text.append('/ibmqx4 - statistics for ibmqx4 quantum processor')
-info_text.append('/ibmqx5 - statistics for ibmqx5 quantum processor')
+info_text.append('/calibration_ibmqx4 - send calibration info')
+info_text.append('/jobs_ibmqx4 - diagram of pending jobs')
+info_text.append('/full_ibmqx4 - calibration and pending jobs info')
+info_text.append('')
+info_text.append('/calibration_ibmqx5 - send calibration info')
+info_text.append('/jobs_ibmqx5 - diagram of pending jobs')
+info_text.append('/full_ibmqx5 - calibration and pending jobs info')
 info_text = '\n'.join(info_text)
 counter = 0
 
@@ -24,24 +29,38 @@ def choose_backend(bot, update):
     counter += 1
 
     update.message.reply_text('Wait a sec ...')
+    user_id = update.message.chat_id
 
-    backend = update.message.text[1:].lower()
+    command = update.message.text.lower()
+    path = None
+    if command == '/calibration_ibmqx4':
+        path = 'tmp/ibmqx4_calibration_full.png'
+    elif command == '/jobs_ibmqx4':
+        path = 'tmp/ibmqx4_jobs_full.png'
+    elif command == '/full_ibmqx4':
+        path = 'tmp/ibmqx4_full.png'
+    elif command == '/calibration_ibmqx5':
+        path = 'tmp/ibmqx5_calibration_full.png'
+    elif command == '/jobs_ibmqx5':
+        path = 'tmp/ibmqx5_jobs_full.png'
+    elif command == '/full_ibmqx5':
+        path = 'tmp/ibmqx5_full.png'
 
-    if backend in utils.backends:
-        # create_statistics(backend)
-
-        user_id = update.message.chat_id
-        bot.send_photo(chat_id=user_id, photo=open('tmp/{}_to_send.png'.format(backend), 'rb'))
-    else:
+    if path is None:
         help(bot, update)
+    else:
+        bot.send_photo(chat_id=user_id, photo=open(path.format(backend), 'rb'))
+    return
 
 
 def info(bot, update):
     update.message.reply_text(counter)
+    return
 
 
 def help(bot, update):
     update.message.reply_text(info_text)
+    return
 
 
 def main():
